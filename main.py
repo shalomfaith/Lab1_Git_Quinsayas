61d27eb006dbba1a96273df9f89a4f0d89186812
shalomfaithquins@gmail.com <shalomfaithquins@gmail.com>
2026-09-25 07:36:36 +0800

    Enable login for Quinsayas branch

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Lab 1: Version Control")
 print("Author: Shalom Faith Quinsayas") # <--- REPLACE WITH YOUR NAME
 print("Student ID: TUPM-26-1928") # <--- REPLACE WITH YOUR ID
+print ("Login Feature Enabled")
